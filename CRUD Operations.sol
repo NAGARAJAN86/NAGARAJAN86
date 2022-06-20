484dc82f17ddea3bae4bb1f113107fba1272bdef
NAGARAJAN86 <103917074+NAGARAJAN86@users.noreply.github.com>
2022-06-20 11:49:53 +0530

    Create CRUD Operations.sol

---
+++ CRUD Operations.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier:MIT
+//CRUD Operations
+pragma solidity >=0.8.4;
+contract Blockchain{
+    uint256 public coins = 0;
+    function add() public{
+        coins+=1;
+    }
+    function subtract() public{
+        coins-=1;
+    }
+     function set(uint256 _coins) public{
+         coins = _coins;
+     }
+
+} 
